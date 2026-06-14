7263366d91f6c9756d390e549a10e4d14574a3d9
ygandhi2611-png <y.gandhi2611@gmail.com>
2026-06-14 08:31:16 -0400

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,12 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
@@ -20,10 +28,9 @@ export default async function handler(req, res) {
     });
 
     const data = await response.json();
-    res.setHeader('Access-Control-Allow-Origin', '*');
     return res.status(200).json(data);
 
   } catch (err) {
     return res.status(500).json({ error: err.message });
   }
-}
+};
